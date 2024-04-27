6a9bb7659a3afdab76dcb7f58866297e95bbfede
Jay <jayaggarwal99288@gmail.com>
2024-04-27 11:25:02 +0530

    jwt token generator

--- Backend/utils/helpers.js
+++ Backend/utils/helpers.js
@@ -1,8 +1,8 @@
 exports={}
-
+const jwt = require("jwt");
 exports.getToken= async (email,user)=>{
     const token = jwt.sign({identifier:user._id});
-     
+     return token;
 }
 
 module.exports=exports
